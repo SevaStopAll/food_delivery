993f6e7fac84bb6d972fa674ee491d5d7f9d0eca
SevaStopAll <Sevastopallrh@yandex.ru>
2023-05-07 16:20:51 +0400

    init comment

---
+++ src/main/java/ru/vsevolod/foodDelivery/OrderApplication.java
@@ -0,0 +1,27 @@
+package ru.vsevolod.foodDelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class OrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderApplication.class, args);
+	}
+
+	@Bean
+	public String returnString() {
+		String str = "http://localhost:8080/dishes/";
+		return str;
+	}
+
+	@Bean
+	public RestTemplate returnRest() {
+		RestTemplate client = new RestTemplate();
+		return client;
+	}
+}
+
